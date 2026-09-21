375eeb9cd3ebb556ba0596736e360ee00460398a
justraph-tech <raphael.maulat@ynov.com>
2026-09-21 14:46:11 +0200

    feat:poisonPot

--- src/potion.go
+++ src/potion.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 const (
-	PotionVie = "potion de vie"
+	PotionVie    = "potion de vie"
+	PotionPoison = "potion de poison"
 )
 
 // takePot utilise une potion de vie sur le personnage.
@@ -31,3 +35,34 @@ func (c *Character) takePot() {
 	fmt.Printf("%s✓ Potion utilisée !%s Vous avez maintenant %s%d/%d PV%s.\n",
 		Green, Reset, Bold, c.Pv, c.PvMax, Reset)
 }
+
+
+func (c *Character) poisonPot() {
+	potQuantity, potCheck := c.Inventaire[PotionPoison]
+
+	if !potCheck || potQuantity <= 0 {
+		fmt.Println(Red + "Vous n'avez pas de potion de poison dans votre inventaire." + Reset)
+		return
+	}
+
+	c.Inventaire[PotionPoison]--
+
+	fmt.Println(Green + "☠ Vous avez bu une potion de poison..." + Reset)
+
+	for second := 1; second <= 3; second++ {
+		time.Sleep(1 * time.Second)
+
+		c.Pv -= 10
+		if c.Pv < 0 {
+			c.Pv = 0
+		}
+
+		fmt.Printf("%s☠ Poison (%d/3)%s : %s%d/%d PV%s\n",
+			Green, second, Reset, Red, c.Pv, c.PvMax, Reset)
+
+		if isDead(c) {
+			return
+		}
+	}
+}
+
