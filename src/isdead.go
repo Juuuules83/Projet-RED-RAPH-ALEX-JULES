55332f73310217646e9873d34a1bfff1bb118617
Jules FARON <jules.faron@ynov.com>
2026-09-21 11:56:45 +0200

    feat : ajout du test de combat

--- src/isdead.go
+++ src/isdead.go
@@ -10,11 +10,7 @@ func isDead(c *Character) bool {
 
 		c.Pv = c.PvMax / 2
 		fmt.Printf("%s est ressuscité avec %d / %d PV\n", c.Name, c.Pv, c.PvMax)
-<<<<<<< HEAD
-	
-=======
 		countdead++
->>>>>>> 0d2d4114f103aa05b6b956a4f9cc89b922b33080
 		return true
 	}
 	if countdead > 0 {
