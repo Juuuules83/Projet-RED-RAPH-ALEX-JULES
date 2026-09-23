dec5031a62888163355a22237ef42027ba7b6c14
Alexandre DEBRABANT <alexandre.debrabant@ynov.com>
2026-09-23 14:53:25 +0200

    fix: marchant ++

--- src/marchant/marchant.go
+++ src/marchant/marchant.go
@@ -54,7 +54,52 @@ func (c *Character) Trader(ItemName string, price int) {
         }else{
             fmt.Println("Pas assez d'argent ou d'espace dans l'inventaire")
         }
-       
+    case 3:
+        countFB := 0
+        if countFB > 0{
+            if c.FireBall >= 75 && player.StockageMax > utils.TotalItems {
+                fmt.Println("Sort : boule de feu [75]")
+                c.AddInventory(combat.FireBall, 1)
+                c.Money -= 75
+                countFB++
+            }else{
+                fmt.Println("Pas assez d'argent ou d'espace dans l'inventaire")
+            }
+        }else{
+            fmt.Print("Sort déja acheté")
+        }
+     case 4:
+        if c.Money >= 4 && player.StockageMax > utils.TotalItems {
+            fmt.Println("fourrure de loup [4]")
+            c.AddInventory(WolfFurr, 1)
+            c.Money -= 4 
+        }else{
+            fmt.Println("Pas assez d'argent ou d'espace dans l'inventaire")
+        }
+     case 5:
+        if c.Money >= 7 && player.StockageMax > utils.TotalItems {
+            fmt.Println("peau de troll [7]")
+            c.AddInventory(TrollSkin, 1)
+            c.Money -= 7
+        }else{
+            fmt.Println("Pas assez d'argent ou d'espace dans l'inventaire")
+        }
+     case 6:
+        if c.Money >= 3 && player.StockageMax > utils.TotalItems {
+            fmt.Println("cuir de sanglier [3]")
+            c.AddInventory(WildBoarLeather, 1)
+            c.Money -= 3
+        }else{
+            fmt.Println("Pas assez d'argent ou d'espace dans l'inventaire")
+        }
+    case 7:
+        if c.Money >= 1 && player.StockageMax > utils.TotalItems {
+            fmt.Println("plume de corbeau [1]")
+            c.AddInventory(CrowFeather, 1)
+            c.Money -= 1
+        }else{
+            fmt.Println("Pas assez d'argent ou d'espace dans l'inventaire")
+        }
     default:
         utils.ClearScreen()
         fmt.Println("Option invalide. Veuillez réessayer.")
