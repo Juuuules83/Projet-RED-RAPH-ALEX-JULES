c3581a54c14619e0b6f08d13f38d7e954b6299df
Jules FARON <jules.faron@ynov.com>
2026-09-23 16:36:28 +0200

    fix : Raph à fait la potion de poison | ajout des ingrédients achetable au marchant pour réaliser les différentes armures

--- src/combat/potion.go
+++ src/combat/potion.go
@@ -2,8 +2,23 @@
 
 package combat
 
-import "projet-red/player"
+import (
+	"projet-red/player"
+	"fmt"
+	"time"
+)
 
 func UtiliserPotionVie(c *player.Character) string {
 	return c.UtiliserPotionVie()	
+}
+
+func PoisonPot(m *Monster) {
+    for i := 0; i < 3; i++ {
+        m.Pv -= 10
+        if m.Pv < 0 {
+            m.Pv = 0
+        }
+        fmt.Printf("%s : %d/%d PV\n", m.Name, m.Pv, m.PvMax)
+        time.Sleep(1 * time.Second)
+    }
 }
